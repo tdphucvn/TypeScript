6d73185c87d63ea13b6c21416dd20b675a1e37d2
Philip Tran <tran.philip02@gmail.com>
2020-11-16 00:22:11 +0100

    Initial TypeScript

---
+++ app.js
@@ -0,0 +1,7 @@
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+var number1 = 12;
+var number2 = 54;
+var result = add(number1, number2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,10 @@
+const add = (n1: number, n2: number) => {
+    return n1 + n2;
+};
+
+const number1 = 12;
+const number2 = 54;
+
+const result = add(number1, number2);
+
+console.log(result);
